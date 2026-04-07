8ca88af33678e08d20fb01937d69b540264d1197
Rodrigo <167549133+Raynagah@users.noreply.github.com>
2026-04-07 15:31:15 -0400

    Limpieza de código

--- gestion-mascotas/src/main/java/com/backend/gestionMascotas/service/ReporteFactory.java
+++ gestion-mascotas/src/main/java/com/backend/gestionMascotas/service/ReporteFactory.java
@@ -4,13 +4,13 @@ package com.backend.gestionMascotas.service;
 import com.backend.gestionMascotas.model.ReporteMascota;
 import org.springframework.stereotype.Component;
 
-@Component // Le dice a Spring que administre esta clase para poder inyectarla
+@Component 
 public class ReporteFactory {
 
-    // Este es nuestro Factory Method
+   
     public ReporteMascota crearReporte(String tipoReporte, String especie, String raza, String color, String tamano, String nombre, String telefono, String email, String fotoUrl, Double lat, Double lng) {
 
-        // Usamos el patrón Builder que nos regaló Lombok en la entidad
+        
         ReporteMascota.ReporteMascotaBuilder builder = ReporteMascota.builder()
                 .especie(especie)
                 .raza(raza)
@@ -23,13 +23,13 @@ public class ReporteFactory {
                 .latitud(lat)
                 .longitud(lng);
 
-        // Lógica de decisión de la fábrica basada en el tipo
+       
         if ("PERDIDA".equalsIgnoreCase(tipoReporte)) {
-            // Aquí en el futuro podrías inicializar atributos específicos para perdidas
+            
             return builder.tipoReporte("PERDIDA").build();
 
         } else if ("ENCONTRADA".equalsIgnoreCase(tipoReporte)) {
-            // Aquí en el futuro podrías inicializar atributos específicos para encontradas
+            
             return builder.tipoReporte("ENCONTRADA").build();
 
         } else {
