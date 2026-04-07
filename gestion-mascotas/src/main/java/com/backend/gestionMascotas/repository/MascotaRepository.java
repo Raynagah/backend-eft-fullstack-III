fe02ace33534b4de77159fd28361148539124ea3
Rodrigo <167549133+Raynagah@users.noreply.github.com>
2026-04-07 15:29:34 -0400

    Limpieza de código

--- gestion-mascotas/src/main/java/com/backend/gestionMascotas/repository/MascotaRepository.java
+++ gestion-mascotas/src/main/java/com/backend/gestionMascotas/repository/MascotaRepository.java
@@ -6,11 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository // Le indica a Spring que este es un componente de acceso a datos
+@Repository 
 public interface MascotaRepository extends JpaRepository<ReporteMascota, Long> {
 
-    // Spring Data JPA hace la "magia" de crear las consultas SQL por ti
-    // Solo con declarar estos métodos, ya puedes buscar en la base de datos:
+    
 
     // Buscar todos los reportes por tipo (Ej: "PERDIDA" o "ENCONTRADA")
     List<ReporteMascota> findByTipoReporte(String tipoReporte);
@@ -18,6 +17,4 @@ public interface MascotaRepository extends JpaRepository<ReporteMascota, Long> {
     // Buscar reportes por especie (Ej: "Perro")
     List<ReporteMascota> findByEspecie(String especie);
 
-    // Buscar reportes por ciudad/ubicación aproximada (Si agregaras ese campo)
-    // List<ReporteMascota> findByCiudad(String ciudad);
 }
