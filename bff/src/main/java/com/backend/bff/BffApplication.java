7b72f929712e55b2ca2589c220aaecc797848636
Raynagah <rodrigo.vargas.rogel99@gmail.com>
2026-05-06 18:23:23 -0400

    Avance 09 en Ms-Bff, adición de Swagger y Swagger vía eureka-server para visualizar en puerto 8080

--- bff/src/main/java/com/backend/bff/BffApplication.java
+++ bff/src/main/java/com/backend/bff/BffApplication.java
@@ -1,5 +1,7 @@
 package com.backend.bff;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Servidor Local")})
 public class BffApplication {
 
 	public static void main(String[] args) {
